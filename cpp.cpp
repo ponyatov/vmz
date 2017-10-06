f7488ce55a657c02c7740f81ee5e142639ca4a71
Dmitry Ponyatov <dponyatov@gmail.com>
2017-10-06 20:50:31 +0400

    + assembler
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,4 +1,49 @@
 #include "hpp.hpp"
+
 #define YYERR "\n\n"<<yylineno<<':'<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() { return yyparse(); } 
+
+int main() {
+	yyparse();					// run assembler/compiler
+	for (;;) current->tick();	// infty execution loop
+}
+
+VM::VM(string N) {
+	name = N;								// set VM name
+	Ip = 0;									// reset instruction pointer
+	Cp = 0;									// reset compilation pointer
+	reg[N]=this;							// register new VM
+}
+
+string VM::head() { return "vm:"+name; }
+string VM::dump() { string S = head(); return S; }
+
+map<string,VM*> VM::reg;
+string VM::reg_dump() { string S = "VM list:";			// dump VM_reg
+	for (auto vm=reg.begin(),e=reg.end();vm!=e;vm++)	// for over registry
+		S += '\n'+vm->second->head();
+	return S; }
+
+void VM::compile(uint8_t C) { M[Cp++]=C; assert(Cp<Msz); }	// compile byte
+
+VM* current=NULL;		// current VM will be used by bytecode executor
+
+void VM::tick() {
+	assert( Ip < Cp );					// check Ip in code < compile point
+	uint8_t cmd = M[Ip++];				// = FETCH command opcode
+	switch (cmd) {									// = DECODE/EXECUTE
+		case NOP: nop(); break;
+		case BYE: bye(); break;
+		default:									// unknown command
+			ostringstream os; os << "bad opcode: "
+				<< "vm:" << name << ' '
+				<< hex << setfill('0') 
+				<< setw(4) << Ip-1 << ':'
+				<< setw(2) << (int)cmd << ' ';
+			yyerror(os.str());
+	}
+}
+
+void VM::nop() {}
+void VM::bye() { exit(0); }
+
--- hpp.hpp
+++ hpp.hpp
@@ -1,18 +1,48 @@
 #ifndef _H_HPP
 #define _H_HPP
 
+								// opcodes
+#define NOP 0x00
+#define BYE 0xFF
+
+#define Msz	0x100				// std.memory init size
+
 #include <iostream>				// = std.includes
+#include <iomanip>
+#include <sstream>
 #include <cstdlib>
+#include <cassert>
+#include <vector>				// variable size arrays
+#include <map>					// symbol tables and registries
 using namespace std;
 
-struct VM {						// = virtual stack machine [wiki:VM]
+struct VM {							// = virtual stack machine [wiki:VM]
+	string name;					// VM name
+	uint8_t M[Msz];					// main memory
+	uint32_t Ip;					// instruction pointer (in place of PC)
+	uint32_t Cp;					// compilation pointer
+	void compile(uint8_t);			// compile bytecode command
+	VM(string);						// constructor
+	string dump();					// = dump VM state
+	string head();					// short dump: name, sizes, state...
+	static map<string,VM*> reg;		// registry (all VMs must be here)
+	static string reg_dump();		// dump registry
+	void tick();					// execute one command:
+	void nop();						// do nothing (filler in code)
+	void bye();						// stop system
 };
 
 								// == lexer/parser interface [wiki:parser]
 extern int yylex();				// = lexer (get next token)
 extern int yylineno;			// current line number
 extern char* yytext;			// lexed text
+#define BC(OP)	{ yylval.cmd = OP; return CMD; }	/* ByteCode */
 extern int yyparse();			// = syntax parser
 extern void yyerror(string);	// syntax error callback
+#include "ypp.tab.hpp"
+
+extern void tick();				// do one command
+extern VM* current;				// current VM will be used by bytecode executor
+extern VM* target;				// target VM will be used by compiler
 
 #endif // _H_HPP
